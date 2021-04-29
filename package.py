81e022ebf72ad366ddf3f98aa30ba0b9902b3f78
udon2301 <shallot2301@gmail.com>
2021-04-29 21:56:41 +0900

    Revert "[add] 3章後半"
    This reverts commit be11f6812024318870eee905119c0cc8693da162.

--- package.py
+++ package.py
@@ -1,5 +1,3 @@
-import re
-
 # 返り値：アルファベットのみの単語リスト
 def alpha_word(string):
     alphabet = []
@@ -45,33 +43,3 @@ def chr_ngram(num, seq):
 #返り値：暗号（9）
 def cipher(seq):
     return ''.join([chr(219 - ord(c)) if c.islower() else c for c in seq])
-
-# 協調マークアップの削除
-def clean_markup(seq):
-    pattern = r'''
-                (\'{2,5})
-                    (.*?)
-                (\1)
-                '''
-    cleaned = re.sub(pattern, r'\2', seq, flags = re.MULTILINE+re.VERBOSE)
-    
-    return cleaned
-
-# 内部リンクマークアップの削除 #27
-# ここが理解できない！！
-def clean_ILmarkup(seq):
-    pattern = r'''
-                \[\[             # '[['(マークアップ開始)
-                (?:              # キャプチャ対象外のグループ開始
-                    [^|]*?       # '|'以外の文字0文字以上、非貪欲(ここがわからん！！！)
-                    \|           # '|'
-                )??              # グループ終了、このグループが0か1出現、非貪欲
-                (                # グループ開始、キャプチャ対象
-                    (?!Category:)  # 否定の先読(含んだ場合は対象外としている)
-                    ([^|]*?)    # '|'以外が0文字以上、非貪欲(表示対象の文字列)
-                )
-                \]\]        # ']]'（マークアップ終了）
-                '''
-    cleaned = re.sub(pattern, r'\1', seq, flags = re.MULTILINE+re.VERBOSE)
-    
-    return cleaned
